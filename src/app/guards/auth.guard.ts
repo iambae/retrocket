4504c92c0f7afb39c8f545730a35f323d4712baa
iambae <jhnbae7@gmail.com>
2020-12-30 23:56:23 -0800

    fix: redirect author to join as new member instead of auth flow

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,24 +1,20 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 
 @Injectable({
   providedIn: "root",
 })
 /**
- * Allows access to requested board only if user data
+ * Allows access to requested board if user data
  * exists in current session, otherwise redirects user
- * to StartComponent to start a new auth flow
+ * to JoinComponent to start join flow
  */
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
-  canActivate(): true | UrlTree {
-    const isLoggedIn = JSON.parse(sessionStorage.getItem("user"));
-
-    if (isLoggedIn) {
-      return true;
-    }
-
-    return this.router.parseUrl("/start");
+  canActivate(route: ActivatedRouteSnapshot): true | UrlTree {
+	const isLoggedIn = JSON.parse(sessionStorage.getItem("user"));
+	
+    return isLoggedIn ? true : this.router.parseUrl(`/join/${route.params.id}`);
   }
 }
